ff2e7690535d88fc0982d20fea35fa77632e08e2
t0yohei <inuneko690121@gmail.com>
2020-06-17 11:14:32 +0900

    actionsを作成

--- src/js/pages/Todos.js
+++ src/js/pages/Todos.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Todo from "../components/Todo";
+import * as TodoActions from "../actions/TodoActions";
 import TodoStore from "../stores/TodoStore";
 
 export default class Todos extends React.Component {
@@ -19,6 +20,10 @@ export default class Todos extends React.Component {
     });
   }
 
+  createTodo() {
+    TodoActions.createTodo("New Todo");
+  }
+
   render() {
     const { todos } = this.state;
 
@@ -28,6 +33,7 @@ export default class Todos extends React.Component {
 
     return (
       <div>
+        <button onClick={this.createTodo.bind(this)}>Create!</button>
         <h1>Todos</h1>
         <ul>{TodoComponents}</ul>
       </div>
