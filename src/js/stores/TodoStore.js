e9e1af702ee9cc52f20f5dbc1aafb0ecc30c253f
t0yohei <inuneko690121@gmail.com>
2020-06-17 10:53:01 +0900

    storeを作成

---
+++ src/js/stores/TodoStore.js
@@ -0,0 +1,39 @@
+import { EventEmitter } from "events";
+
+class TodoStore extends EventEmitter {
+  constructor() {
+    super();
+    this.todos = [
+      {
+        id: 113464613,
+        text: "Go Shopping",
+        complete: false,
+      },
+      {
+        id: 235684679,
+        text: "Pay Bills",
+        complete: false,
+      },
+    ];
+  }
+
+  createTodo(text) {
+    const id = Date.now();
+
+    this.todos.push({
+      id,
+      text,
+      complete: false,
+    });
+
+    this.emit("change");
+  }
+
+  getAll() {
+    return this.todos;
+  }
+}
+
+const todoStore = new TodoStore();
+
+export default todoStore;
